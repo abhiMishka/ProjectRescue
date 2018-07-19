ca581431bf3a26fe75cfd1e8c2938ea0ec220c43
Harsh <srivastavaharsh9888@gmail.com>
2018-07-19 18:23:04 +0530

    Saving of messages added

--- sendnoti/models.py
+++ sendnoti/models.py
@@ -5,3 +5,10 @@ class UserDetail(models.Model):
     phone_number=models.CharField(max_length=50)
     firebase_token = models.TextField(default=None, null=True)
     detail=models.TextField()
+    
+class Message(models.Model):
+    user=models.ForeignKey(UserDetail,on_delete=models.CASCADE)
+    message=models.TextField()
+    
+    def __str__(self):
+        return (str(self.user.name)+' (' + str(self.user.phone_number)+') --> ' + str(self.message))
---
+++ sendnoti/models.py~
@@ -0,0 +1,7 @@
+from django.db import models
+
+class UserDetail(models.Model):
+    name=models.CharField(max_length=250)
+    phone_number=models.CharField(max_length=50)
+    firebase_token = models.TextField(default=None, null=True)
+    detail=models.TextField()
