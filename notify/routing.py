2a6d4a408325a4a8492ab12382e265fbac6bf5fd
Harsh <srivastavaharsh9888@gmail.com>
2018-08-03 13:36:27 +0530

    Added all again

---
+++ notify/routing.py
@@ -0,0 +1,13 @@
+from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import socket_sendnoti.routing
+
+application = ProtocolTypeRouter({
+    # (http->django views is added by default)
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            socket_sendnoti.routing.websocket_urlpatterns
+        )
+    ),
+})
